58d13f2b703bc2703cb3b748634cf922e0cfe7ac
MunnaJaiswal <codermunna007@gmail.com>
2024-12-26 19:00:09 +0545

    making button color changeable when it was clicked

--- lib/views/category_screen/item_details.dart
+++ lib/views/category_screen/item_details.dart
@@ -1,31 +1,44 @@
 import 'package:eecm/consts/consts.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import '../../widgets_common/background_widget.dart';
-import 'item_details.dart';
 
-class ItemsDetails extends StatelessWidget {
+class ItemsDetails extends StatefulWidget {
   final String? title;
 
   const ItemsDetails({super.key, required this.title});
+
+  @override
+  State<ItemsDetails> createState() => _ItemsDetailsState();
+}
+
+class _ItemsDetailsState extends State<ItemsDetails> {
+  bool _isClickable = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightGrey,
       appBar: AppBar(
-        title: Text(title!,style: TextStyle(color: darkFontGrey),),
+        title: Text(
+          widget.title!,
+          style: TextStyle(color: darkFontGrey),
+        ),
         actions: [
           IconButton(
-              onPressed: () {
-              },
-              icon: Icon(Icons.share,color: darkFontGrey,),
+            onPressed: () {},
+            icon: Icon(
+              Icons.share,
+              color: darkFontGrey,
+            ),
           ),
           IconButton(
-              onPressed: () {
-                Get.back();
-              },
-              icon: Icon(Icons.favorite_border,color: darkFontGrey,),
+            onPressed: () {
+              setState(() {
+                _isClickable = !_isClickable;
+              });
+            },
+            icon: Icon(
+              _isClickable ? Icons.favorite : Icons.favorite_border,
+              color: _isClickable ? Colors.red : darkFontGrey,
+            ),
           )
         ],
       ),
